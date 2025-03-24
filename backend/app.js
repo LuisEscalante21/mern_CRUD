22f0a5ba3706ebd8e6a7a84bcf9a8cc23d65b735
LuisEscalante <20200602@ricaldone.edu.sv>
2025-03-24 11:25:43 -0600

    arregle para que aceptara  cookies

--- backend/app.js
+++ backend/app.js
@@ -6,8 +6,9 @@ import employeesRoutes from "./src/routes/employees.js";
 import branchesRoutes from "./src/routes/branches.js"
 import reviewsRoutes from "./src/routes/reviews.js";
 import evaluationsRoutes from "./src/routes/evaluations.js";
-import registerEmployeesRoutes from "./src/controllers/registerEmployeesController.js";
-import loginRoutes from "./src/controllers/loginController.js";
+import registerEmployeesRoutes from "./src/routes/registerEmployees.js";
+import loginRoutes from "./src/routes/login.js";
+import cookieParser from "cookie-parser";
 
 // Creo una constante que es igual
 // a la libreria que importé y la ejecuta
@@ -16,10 +17,13 @@ const app = express();
 // Uso un middleware para que acepte datos Json
 app.use(express.json());
 
+// que acepte cookies
+app.use(cookieParser());
+
 // Definir la ruta
 app.use("/api/products", productsRoutes);
 app.use("/api/clients", clientsRoutes);
-app.use("/api/employees", employeesRoutes); º
+app.use("/api/employees", employeesRoutes); 
 app.use("/api/branches", branchesRoutes);
 app.use("/api/reviews", reviewsRoutes);
 app.use("/api/evaluations", evaluationsRoutes);
--- backend/src/routes/login.js
+++ backend/src/routes/login.js
@@ -1,8 +1,8 @@
 import express from "express";
-import loginController from "../controllers/loginController";
+import loginController from "../controllers/loginController.js";
 
 const router = express.Router();
 
-router.route("/").post(loginController);
+router.route("/").post(loginController.login);
 
 export default router;
