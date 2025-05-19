5d37131ee27f24be706281dc77646923f5d35ff5
luiescalante21 <20200602@ricaldone.edu.sv>
2025-05-19 11:18:51 -0600

    agregue el middleware de validación de token de autenticación para empleados

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ import logoutRoutes from "./src/routes/logout.js"
 import registerClientsRoutes from "./src/routes/registerClients.js";
 import passwordRecoveryRoutes from "./src/routes/passwordRecovery.js";
 import blogRoutes from "./src/routes/blog.js";
-
+import { validateAuthToken } from "./src/middlewares/validateAuthToken.js";
 // Creo una constante que es igual
 // a la libreria que importé y la ejecuta
 const app = express();
@@ -27,7 +27,7 @@ app.use(cookieParser());
 // Definir la ruta
 app.use("/api/products", productsRoutes);
 app.use("/api/clients", clientsRoutes);
-app.use("/api/employees", employeesRoutes); 
+app.use("/api/employees", validateAuthToken(["employee", "Admin"]), employeesRoutes); 
 app.use("/api/branches", branchesRoutes);
 app.use("/api/reviews", reviewsRoutes);
 app.use("/api/evaluations", evaluationsRoutes);
---
+++ backend/src/middlewares/validateAuthToken.js
@@ -0,0 +1,27 @@
+import jsonwebtoken from 'jsonwebtoken';
+import { config } from '../config.js';
+
+export const validateAuthToken = (allowedUserTypes = []) => {}
+
+    return (req, res, next) => {
+        try {
+            if (!req.cookies){
+            return res.json({ message: 'No token provided' });
+            }
+
+            const { authToken } = req.cookies;
+            
+            const decoded = jsonwebtoken.verify(authToken, config.JWT_SECRET);
+
+            req.user = decoded;
+
+            if(!allowedUserTypes.includes(decoded.userType)) {
+                return res.json({ message: 'access denied' });
+            }
+
+            next()
+            
+        } catch (error) {
+            return res.status(401).json({ message: 'Invalid token' });
+        }
+    }
